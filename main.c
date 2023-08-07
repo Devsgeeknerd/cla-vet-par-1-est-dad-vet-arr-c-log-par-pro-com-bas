590a97866704e52107d5086dbfda8529d03de45f
Paulo Vanderley <vanderley.1109@gmail.com>
2023-08-07 17:26:45 -0300

    adicionando conteudo

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numerosDigitados[10];
+    int i = 0;
+    for (i = 0; i <= 9; i++)
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &numerosDigitados[1]);
+        return 0;
+    }
+}
